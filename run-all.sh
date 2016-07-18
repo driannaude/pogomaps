1fa531548c9c15eba1646eba74be839c1282be6d
Drian Naude <drian.naude@truescape.com>
2016-07-19 11:06:43 +1200

    Fixed file overwrite issue

--- run-all.sh
+++ run-all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #python -m SimpleHTTPServer 8000 &>/dev/null &
-python main.py -u chchhagley -p hagleyvulpix -l "8 Riccarton Ave, Christchurch Central, Christchurch" &
-python main.py -u chchsumner -p sumnerpikachu -l "2 Esplanade, Sumner, Christchurch" &
-python main.py -u chchbrighton -p brightonsnorlax -l "214 Marine Parade, New Brighton, Christchurch" &
+python main.py -u chchhagley -p hagleyvulpix -l "8 Riccarton Ave, Christchurch Central, Christchurch" -f "data.hagley.json" &
+python main.py -u chchsumner -p sumnerpikachu -l "2 Esplanade, Sumner, Christchurch" -f "data.sumner.json" &
+python main.py -u chchbrighton -p brightonsnorlax -l "214 Marine Parade, New Brighton, Christchurch" -f "data.brighton.json" &
