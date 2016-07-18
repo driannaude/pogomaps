b974ce247f41b8f81e8595255bd86a7a0258a5be
Drian Naude <drian.naude@truescape.com>
2016-07-19 10:13:11 +1200

    Added run-all command to spawn all search areas

---
+++ run-all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#python -m SimpleHTTPServer 8000 &>/dev/null &
+python main.py -u chchhagley -p hagleyvulpix -l "8 Riccarton Ave, Christchurch Central, Christchurch"
+python main.py -u chchsumner -p sumnerpikachu -l "2 Esplanade, Sumner, Christchurch"
+python main.py -u chchbrighton -p brightonsnorlax -l "214 Marine Parade, New Brighton, Christchurch"
+trap "killall Python" EXIT
