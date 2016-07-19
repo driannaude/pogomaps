6ac6893303fe82369cf8cede17ef29987303b572
Drian Naude <drian.naude@truescape.com>
2016-07-19 16:54:28 +1200

    Corrected pw

--- run-all.sh
+++ run-all.sh
@@ -4,4 +4,4 @@ python main.py -u chchhagley -p hagleyvulpix -l "8 Riccarton Ave, Christchurch C
 python main.py -u chchsumner -p sumnerpikachu -l "2 Esplanade, Sumner, Christchurch" -f "data.sumner.json" &
 python main.py -u chchbrighton -p brightonsnorlax -l "214 Marine Parade, New Brighton, Christchurch" -f "data.brighton.json" &
 python main.py -u chchriccarton -p riccartonzubat -l "122 Riccarton Road, Riccarton, Christchurch" -f "data.riccarton.json" &
-python main.py -u chchaddington -p addingtonclefairy -l "485 Barington Street, Addington, Christchurch" -f "data.addington.json" &
+python main.py -u chchaddington -p addingtonclefai -l "485 Barington Street, Addington, Christchurch" -f "data.addington.json" &
