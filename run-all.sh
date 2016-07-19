c04aca34a0b3d4a93d04e31b3e1ca795b5bcb270
Drian Naude <drian.naude@truescape.com>
2016-07-19 19:57:27 +1200

    Added debug

--- run-all.sh
+++ run-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #python -m SimpleHTTPServer 8000 &>/dev/null &
-python main.py -u chchhagley -p hagleyvulpix -l "8 Riccarton Ave, Christchurch Central, Christchurch" -f "data.hagley.json" &
-python main.py -u chchsumner -p sumnerpikachu -l "2 Esplanade, Sumner, Christchurch" -f "data.sumner.json" &
-python main.py -u chchbrighton -p brightonsnorlax -l "214 Marine Parade, New Brighton, Christchurch" -f "data.brighton.json" &
-python main.py -u chchriccarton -p riccartonzubat -l "122 Riccarton Road, Riccarton, Christchurch" -f "data.riccarton.json" &
-python main.py -u chchaddington -p addingtonzubat -l "485 Barington Street, Addington, Christchurch" -f "data.addington.json" &
+python main.py -d -u chchhagley -p hagleyvulpix -l "8 Riccarton Ave, Christchurch Central, Christchurch" -f "data.hagley.json" &
+python main.py -d -u chchsumner -p sumnerpikachu -l "2 Esplanade, Sumner, Christchurch" -f "data.sumner.json" &
+python main.py -d -u chchbrighton -p brightonsnorlax -l "214 Marine Parade, New Brighton, Christchurch" -f "data.brighton.json" &
+python main.py -d -u chchriccarton -p riccartonzubat -l "122 Riccarton Road, Riccarton, Christchurch" -f "data.riccarton.json" &
+python main.py -d -u chchaddington -p addingtonzubat -l "485 Barington Street, Addington, Christchurch" -f "data.addington.json" &
