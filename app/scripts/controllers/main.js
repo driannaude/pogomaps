ecee7d48532a6ac875a09c9fc263dcdb50c81789
Drian Naude <drian.naude@truescape.com>
2016-07-21 20:09:13 +1200

    Added despawn logic to frontend

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -24,22 +24,23 @@ angular.module('ngApp')
     $scope.infoWindows = [];
     $scope.pokemonList = [];
     $scope.radioModel = 'All';
-    $scope.select = function(value){
-      _.each($scope.pokemonList, function(pokemon){
+    $scope.select = function(value) {
+      _.each($scope.pokemonList, function(pokemon) {
         pokemon.active = value;
       });
     };
-    $scope.visiblePokemon = function(){
+    $scope.visiblePokemon = function() {
       var returnList = [];
-      _.each($scope.markers, function(marker){
-        _.each($scope.pokemonList, function(pokemon){
-          if(pokemon.name === marker.pokemon.name && pokemon.active === true){
+      _.each($scope.markers, function(marker) {
+        _.each($scope.pokemonList, function(pokemon) {
+          if (pokemon.name === marker.pokemon.name && pokemon.active === true) {
             returnList.push(marker);
           }
         });
       });
       return returnList;
     };
+
     function _cap(string) {
       return string.charAt(0).toUpperCase() + string.slice(1);
     }
@@ -52,75 +53,88 @@ angular.module('ngApp')
 
     function main() {
       _getPokemonData();
-      $interval(function(){
+      $interval(function() {
         _getPokemonData();
-      },5000);
-
+      }, 5000);
     }
 
-    function _getPokemonIcon(p){
-        var name = p.name;
-        if (p.name === 'Nidoran F') {
-          name = 'Nidoran';
-        }
-        else if (name === 'Nidoran M') {
-          name = 'Nidorano';
-        }
-        return 'http://icons.iconarchive.com/icons/hektakun/pokemon/72/' + _pad(p.id, 3) + '-' + _cap(name) + '-icon.png';
-
+    function _getPokemonIcon(p) {
+      var name = p.name;
+      if (p.name === 'Nidoran F') {
+        name = 'Nidoran';
+      } else if (name === 'Nidoran M') {
+        name = 'Nidorano';
+      }
+      return 'http://icons.iconarchive.com/icons/hektakun/pokemon/72/' + _pad(p.id, 3) + '-' + _cap(name) + '-icon.png';
     }
+
+    function removeStalePokemonMarkers() {
+      var ts = moment();
+      _.each($scope.markers, function(marker, index) {
+        if(!marker){
+          return;
+        }
+        var pokemon = marker.pokemon;
+        var tsLeft = moment((pokemon.timestamp + pokemon.timeleft) * 1000);
+        if (!tsLeft.isAfter(ts)) {
+          console.log('Removing ' + pokemon.name);
+          $scope.markers.splice(index, 1);
+        }
+      });
+    }
+
     function _processPokemons(pokemons) {
-      _.each(pokemons, function(pokemon){
+      removeStalePokemonMarkers();
+      _.each(pokemons, function(pokemon) {
         var ts = moment();
         var tsLeft = moment((pokemon.timestamp + pokemon.timeleft) * 1000);
-
-        if(!tsLeft.isAfter(ts)){
+        if (!tsLeft.isAfter(ts)) {
           return false;
         }
         var pokeId = $scope.markers.length - 1;
-        if(pokeId < 0){
+        if (pokeId < 0) {
           pokeId = 0;
         }
-      var newMarker = {
-        id: pokeId,
-        coords: {
-          latitude: pokemon.lat,
-          longitude: pokemon.lng
-        },
-        options: {
-          draggable: false,
-          icon: _getPokemonIcon(pokemon)
-        },
-        pokemon: pokemon,
-        infoWindow: {
-          show: false,
+        var newMarker = {
+          id: pokeId,
+          coords: {
+            latitude: pokemon.lat,
+            longitude: pokemon.lng
+          },
           options: {
-            content: '<strong>' + pokemon.name + '</strong><br />' + tsLeft.fromNow(ts) + ' until despawn<br /><span class="text-muted">Lat: '+ pokemon.lat +'</span><br /><span class="text-muted">Lng: '+ pokemon.lng + '</span>',
+            draggable: false,
+            icon: _getPokemonIcon(pokemon)
+          },
+          pokemon: pokemon,
+          infoWindow: {
+            show: false,
+            options: {
+              content: '<strong>' + pokemon.name + '</strong><br />' + tsLeft.fromNow(ts) + ' until despawn<br /><span class="text-muted">Lat: ' + pokemon.lat + '</span><br /><span class="text-muted">Lng: ' + pokemon.lng + '</span>',
+            }
+          },
+        };
+        var existingMarkerIndex = _.findIndex($scope.markers, function(o) {
+          return (o.coords.latitude === newMarker.coords.latitude && o.coords.longitude === newMarker.coords.longitude);
+        });
+        if (existingMarkerIndex < 0) {
+          $scope.markers.push(newMarker);
+          var indexInList = _.findIndex($scope.pokemonList, {
+            name: pokemon.name
+          });
+          if (indexInList < 0) {
+            var pkmn = {
+              count: 1,
+              name: pokemon.name,
+              active: true
+            };
+            $scope.pokemonList.push(pkmn);
+          } else {
+            $scope.pokemonList[indexInList].count++;
           }
-        },
-      };
-
-      var existingMarkerIndex = _.findIndex($scope.markers, function(o){
-        return (o.coords.latitude === newMarker.coords.latitude && o.coords.longitude === newMarker.coords.longitude);
-      });
-      if(existingMarkerIndex < 0){
-        $scope.markers.push(newMarker);
-        var indexInList = _.findIndex($scope.pokemonList, {name: pokemon.name});
-        if(indexInList < 0){
-          var pkmn = {
-            count: 1,
-            name: pokemon.name,
-            active: true
-          };
-          $scope.pokemonList.push(pkmn);
         } else {
-          $scope.pokemonList[indexInList].count++;
+          $scope.markers[existingMarkerIndex].infoWindow.options.content = '<strong>' + pokemon.name + '</strong><br />' + tsLeft.fromNow(ts) + ' until despawn<br /><span class="text-muted">Lat: ' + pokemon.lat + '</span><br /><span class="text-muted">Lng: ' + pokemon.lng + '</span>';
         }
-      } else {
-        $scope.markers[existingMarkerIndex].infoWindow.options.content = '<strong>' + pokemon.name + '</strong><br />' + tsLeft.fromNow(ts) + ' until despawn<br /><span class="text-muted">Lat: '+ pokemon.lat +'</span><br /><span class="text-muted">Lng: '+ pokemon.lng + '</span>';
-      }
-    });
-
+      });
     }
 
     function _getPokemonData() {
