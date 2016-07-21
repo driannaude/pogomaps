4fc296200e5f6a9582de18c6ba65f4d8e540c2d0
Drian Naude <drian.naude@truescape.com>
2016-07-21 19:45:41 +1200

    Fix for shithouse angular-generator

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -19,6 +19,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'https://maps.googleapis.com/maps/api/js?sensor=false',
       // bower:js
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
